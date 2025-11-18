ec41116563d40054ae9579e002d16b3fcb96d72d
Jess Hendrix <dev@penguini3.anonaddy.me>
2025-11-18 14:04:44 -0600

    feat: add hyfetch

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,6 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 zoxide init fish --cmd cd | source
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+  hyfetch
+end
