364d5c1c071ab38d33f97d23b15565b6c09ac036
Jess Hendrix <dev@penguini3.anonaddy.me>
2025-11-19 13:01:21 -0600

    fix

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 zoxide init fish --cmd cd | source
+set -gx LEDGER_FILE /home/penguini3/Finance/2025.journal
 # overwrite greeting
 # potentially disabling fastfetch
 function fish_greeting
